2a635eef7cec06c529cdb0a89b3bac865c6d6cf5
Anil Raj Rimal <119393936+anilrajrimal1@users.noreply.github.com>
2025-05-29 14:26:18 +0545

    chore(pipy): bump version to v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sharemydir",
-    version="1.0.1",
+    version="1.0.2",
     description="Instantly serve any folder over HTTP with zero configuration and one-click downloads.",
     long_description="A tiny cross-platform Python CLI tool to instantly serve any folder over HTTP. Supports local IP discovery, QR code display for mobile access, folder downloads as ZIP files, and a user-friendly web UI.",
     long_description_content_type="text/plain",
